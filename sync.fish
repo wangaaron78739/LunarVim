b475b45157ad6b0299c862e1e6ce7202f68cd53a
Anshuman <amedhi@connect.ust.hk>
2021-05-26 20:13:58 +0800

    fix the sync script

--- sync.fish
+++ sync.fish
@@ -3,8 +3,9 @@
 set oldwd (pwd)
 cd ~/.config/nvim/
 
-git add . && git commit -v && yadm add . && yadm commit -m "update nvim config"
-git fetch --all && git pull && git push
+git add . && git commit -v && fish -c 'cd .. && yadm add nvim && yadm commit -m "update nvim config"'
+git fetch --all && git pull 
 git merge --no-ff upstream/master master
+git push
 
 cd $oldwd
