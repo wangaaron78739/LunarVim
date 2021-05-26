f86cae6b00aa6bdbd14ba5e9ae803c69486f95c6
Anshuman <amedhi@connect.ust.hk>
2021-05-26 20:10:46 +0800

    automatically yadm

--- sync.fish
+++ sync.fish
@@ -3,7 +3,7 @@
 set oldwd (pwd)
 cd ~/.config/nvim/
 
-git add . && git commit -v
+git add . && git commit -v && yadm add . && yadm commit -m "update nvim config"
 git fetch --all && git pull && git push
 git merge --no-ff upstream/master master
 
