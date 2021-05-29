51e2b6859191f6e8e50b5ccddc6ec956c313d999
Anshuman <amedhi@connect.ust.hk>
2021-05-29 13:10:48 +0800

    more verbose sync script

--- sync.fish
+++ sync.fish
@@ -3,9 +3,20 @@
 set oldwd (pwd)
 cd ~/.config/nvim/
 
-git add . && git commit -v && fish -c 'cd .. && yadm add nvim && yadm commit -m "update nvim config"'
+echo 'git add . && git commit -v' # '&& fish -c cd .. && yadm add nvim && yadm commit -m "merge upstream lunarvim"'
+git add . && git commit -v # && fish -c 'cd .. && yadm add nvim && yadm commit -m "merge upstream lunarvim"'
+
+echo 'git fetch --all && git pull'
 git fetch --all && git pull 
-git merge --no-ff upstream/master master && fish -c 'cd .. && yadm add nvim && yadm commit -m "update nvim config"' 
+
+echo 'git merge --no-ff upstream/master master' # '&& fish -c cd .. && yadm add nvim && yadm commit -m "update nvim config"'
+git merge --no-ff upstream/master master # && fish -c 'cd .. && yadm add nvim && yadm commit -m "merge upstream lunarvim"' 
+
+echo 'fish -c cd .. && yadm add nvim && yadm commit -m "merge upstream lunarvim"'
+fish -c 'cd .. && yadm add nvim && yadm commit -m "merge upstream lunarvim"' 
+
+echo 'git push'
 git push
+echo 'remember to yadm push'
 
 cd $oldwd
