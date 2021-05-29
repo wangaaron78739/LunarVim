ac534e0163673963a0d767b1a54b86935b955626
Anshuman <amedhi@connect.ust.hk>
2021-05-29 21:57:20 +0800

    tweak treesitter-objects

--- lua/lv-treesitter/init.lua
+++ lua/lv-treesitter/init.lua
@@ -23,10 +23,13 @@ require'nvim-treesitter.configs'.setup {
         swap = {
           enable = true,
           swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
+            ["<leader>aa"] = "@parameter.inner",
+            ["<leader>af"] = "@function.inner",
+            ["<leader>ac"] = "@class.inner",
+            ["<leader>ak"] = "@block.inner",
+            ["<leader>ai"] = "@conditional.inner",
+            ["<leader>aC"] = "@call.inner",
+            ["<leader>al"] = "@loop.inner",
           },
         },
         move = {
@@ -35,18 +38,22 @@ require'nvim-treesitter.configs'.setup {
           goto_next_start = {
             ["]m"] = "@function.outer",
             ["]]"] = "@class.outer",
+            ["]a"] = "@parameter.outer",
           },
           goto_next_end = {
             ["]M"] = "@function.outer",
             ["]["] = "@class.outer",
+            ["]A"] = "@parameter.outer",
           },
           goto_previous_start = {
             ["[m"] = "@function.outer",
             ["[["] = "@class.outer",
+            ["]a"] = "@parameter.outer",
           },
           goto_previous_end = {
             ["[M"] = "@function.outer",
             ["[]"] = "@class.outer",
+            ["]a"] = "@parameter.outer",
           },
         },
         select = {
@@ -57,12 +64,12 @@ require'nvim-treesitter.configs'.setup {
             ["if"] = "@function.inner",
             ["ac"] = "@class.outer",
             ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
+            -- ["aa"] = "@parameter.outer",
             ["ia"] = "@parameter.inner",
             ["ak"] = "@block.outer",
             ["ik"] = "@block.inner",
-            ["ac"] = "@conditional.outer",
-            ["ic"] = "@conditional.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
             ["aC"] = "@call.outer",
             ["iC"] = "@call.inner",
             ["al"] = "@loop.outer",
