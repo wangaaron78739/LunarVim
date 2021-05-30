da3608f9f4ccf027875f88328b9071329e7e8594
Anshuman <amedhi@connect.ust.hk>
2021-05-30 13:21:37 +0800

    configure neosroll

--- init.lua
+++ init.lua
@@ -76,7 +76,16 @@ require("trouble").setup {
     hover = "h"
 }
 
-require('neoscroll').setup()
+require('neoscroll').setup({
+    -- All these keys will be mapped to their corresponding default scrolling animation
+    mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+    hide_cursor = false,          -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = true,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    easing_function = "sine"        -- Default easing function
+})
 require('todo-comments').setup()
 require('diffview').setup()
 
