e66c15d25a9ca17e13b16c9ac8b5c855be6ed911
Anshuman <amedhi@connect.ust.hk>
2021-06-02 16:59:45 +0800

    latexmk compiler options

--- lua/settings.lua
+++ lua/settings.lua
@@ -47,6 +47,15 @@ vim.g.tex_flavor='latex'
 vim.g.vimtex_view_method='zathura'
 vim.g.vimtex_quickfix_mode=0
 vim.g.tex_conceal = 'abdmgs'
+vim.g.vimtex_compiler_latexmk =  {
+     ['options'] = {
+       '-shell-escape',
+       '-verbose',
+       '-file-line-error',
+       '-synctex=1',
+       '-interaction=nonstopmode',
+     },
+}
 
 -- quick-scope
 -- vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
