be3be545d9ca92dbb7045d07266048978d0ecb22
Anshuman <amedhi@connect.ust.hk>
2021-06-12 22:00:53 +0800

    use tectonic

--- lua/settings.lua
+++ lua/settings.lua
@@ -47,6 +47,7 @@ vim.g.tex_flavor='latex'
 vim.g.vimtex_view_method='zathura'
 vim.g.vimtex_quickfix_mode=0
 vim.g.tex_conceal = 'abdmgs'
+vim.g.vimtex_compiler_method ="tectonic"
 vim.g.vimtex_compiler_latexmk =  {
      ['options'] = {
        '-shell-escape',
