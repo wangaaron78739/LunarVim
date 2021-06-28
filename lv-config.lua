d7592f4ac8e514d7c607001802feaa20d31740ce
Anshuman <amedhi@connect.ust.hk>
2021-06-28 15:01:53 +0800

    Woops restore my lv-config.lua

--- lv-config.lua
+++ lv-config.lua
@@ -8,12 +8,12 @@ an executable
 ]] -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 -- general
 O.auto_complete = true
-O.colorscheme = 'lunar'
+O.colorscheme = 'snazzy'
 O.auto_close_tree = 0
 O.wrap_lines = false
 O.timeoutlen = 100
 O.document_highlight = true
-O.extras = false
+O.extras = true
 O.leader_key = ' '
 
 -- After changing plugin config it is recommended to run :PackerCompile
@@ -25,7 +25,7 @@ O.plugins.hop.active = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = "all"
-O.treesitter.ignore_install = {"haskell"}
+-- O.treesitter.ignore_install = {"haskell"}
 O.treesitter.highlight.enabled = true
 
 O.lang.clang.diagnostics.virtual_text = false
@@ -36,7 +36,7 @@ O.lang.clang.diagnostics.underline = false
 -- add things like O.python.formatter.yapf.exec_path
 -- add things like O.python.linter.flake8.exec_path
 -- add things like O.python.formatter.isort.exec_path
-O.lang.python.formatter = 'yapf'
+O.lang.python.formatter = 'black'
 -- O.python.linter = 'flake8'
 O.lang.python.active = true
 O.lang.python.isort = true
@@ -53,11 +53,11 @@ O.lang.python.analysis.use_library_code_types = true
 O.lang.lua.active = true
 O.lang.lua.formatter = 'lua-format'
 -- O.lua.formatter = 'lua-format'
-O.lang.lua.autoformat = false
+O.lang.lua.autoformat = true
 
 -- javascript
 O.lang.tsserver.formatter = 'prettier'
-O.lang.tsserver.linter = nil
+O.lang.tsserver.linter = 'eslint'
 O.lang.tsserver.autoformat = true
 
 -- json
@@ -72,6 +72,15 @@ O.lang.go.autoformat = true
 
 -- Turn off relative_numbers
 -- O.relative_number = false
+
+-- neovide settings
+-- vim.g.neovide_cursor_vfx_mode = "railgun"
+-- vim.g.neovide_refresh_rate=120
+
+-- My settings
+-- vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.scrolloff = 10
 
 -- Turn off cursorline
 -- O.cursorline = false
