ae3b38d4d8927e9ee6c25c293aba4b4b916c94de
christianchiarulli <chrisatmachine@gmail.com>
2021-06-28 02:37:33 -0400

    more optional plugins

--- lua/lv-dashboard/init.lua
+++ lua/lv-dashboard/init.lua
@@ -1,46 +1,68 @@
--- vim.g.dashboard_custom_header = {
---     '███╗   ██╗██╗   ██╗ ██████╗ ██████╗ ██████╗ ███████╗',
---     '████╗  ██║██║   ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝',
---     '██╔██╗ ██║██║   ██║██║     ██║   ██║██║  ██║█████╗',
---     '██║╚██╗██║╚██╗ ██╔╝██║     ██║   ██║██║  ██║██╔══╝',
---     '██║ ╚████║ ╚████╔╝ ╚██████╗╚██████╔╝██████╔╝███████╗',
---     '╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝'
--- }
--- vim.g.dashboard_custom_header = {
---
--- '      _..._                                                                           ',
--- '    .\'     `.   ██      ██    ██ ███    ██  █████  ██████  ██    ██ ██ ███    ███ ',
--- '   :         :  ██      ██    ██ ████   ██ ██   ██ ██   ██ ██    ██ ██ ████  ████ ',
--- '   :         :  ██      ██    ██ ██ ██  ██ ███████ ██████  ██    ██ ██ ██ ████ ██ ',
--- '   `.       .\'  ██      ██    ██ ██  ██ ██ ██   ██ ██   ██  ██  ██  ██ ██  ██  ██ ',
--- '     `-...-\'    ███████  ██████  ██   ████ ██   ██ ██   ██   ████   ██ ██      ██ ',
---
--- }
+local M = {}
 
-vim.g.dashboard_custom_header = O.dashboard.custom_header
+M.config = function()
+    -- vim.g.dashboard_custom_header = {
+    --     '███╗   ██╗██╗   ██╗ ██████╗ ██████╗ ██████╗ ███████╗',
+    --     '████╗  ██║██║   ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝',
+    --     '██╔██╗ ██║██║   ██║██║     ██║   ██║██║  ██║█████╗',
+    --     '██║╚██╗██║╚██╗ ██╔╝██║     ██║   ██║██║  ██║██╔══╝',
+    --     '██║ ╚████║ ╚████╔╝ ╚██████╗╚██████╔╝██████╔╝███████╗',
+    --     '╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝'
+    -- }
+    -- vim.g.dashboard_custom_header = {
+    --
+    -- '      _..._                                                                           ',
+    -- '    .\'     `.   ██      ██    ██ ███    ██  █████  ██████  ██    ██ ██ ███    ███ ',
+    -- '   :         :  ██      ██    ██ ████   ██ ██   ██ ██   ██ ██    ██ ██ ████  ████ ',
+    -- '   :         :  ██      ██    ██ ██ ██  ██ ███████ ██████  ██    ██ ██ ██ ████ ██ ',
+    -- '   `.       .\'  ██      ██    ██ ██  ██ ██ ██   ██ ██   ██  ██  ██  ██ ██  ██  ██ ',
+    -- '     `-...-\'    ███████  ██████  ██   ████ ██   ██ ██   ██   ████   ██ ██      ██ ',
+    --
+    -- }
+    vim.g.dashboard_disable_at_vimenter = 0
 
-vim.g.dashboard_default_executive = 'telescope'
+    vim.g.dashboard_custom_header = O.dashboard.custom_header
 
-vim.g.dashboard_custom_section = {
-    a = {description = {'  Find File          '}, command = 'Telescope find_files'},
-    b = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
-    c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
-    d = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    e = {description = {'  Settings           '}, command = ':e '..CONFIG_PATH..'/lv-settings.lua'}
-    -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
-}
+    vim.g.dashboard_default_executive = 'telescope'
 
--- file_browser = {description = {' File Browser'}, command = 'Telescope find_files'},
+    vim.g.dashboard_custom_section = {
+        a = {
+            description = {'  Find File          '},
+            command = 'Telescope find_files'
+        },
+        b = {
+            description = {'  Recently Used Files'},
+            command = 'Telescope oldfiles'
+        },
+        c = {
+            description = {'  Load Last Session  '},
+            command = 'SessionLoad'
+        },
+        d = {
+            description = {'  Find Word          '},
+            command = 'Telescope live_grep'
+        },
+        e = {
+            description = {'  Settings           '},
+            command = ':e ' .. CONFIG_PATH .. '/lv-config.lua'
+        }
+        -- e = {description = {'  Marks              '}, command = 'Telescope marks'}
+    }
 
--- TODO: this doesnt work!?
-vim.g.dashboard_custom_shortcut = {
-    a = 'f',
-    b = 'SPC f a',
-    c = 'SPC s l',
-    d = 'SPC c n',
-    e = 'SPC f b'
-}
--- find_history = 'SPC f h',
+    -- file_browser = {description = {' File Browser'}, command = 'Telescope find_files'},
 
--- vim.g.dashboard_session_directory = CACHE_PATH..'/session'
-vim.g.dashboard_custom_footer = O.dashboard.footer
+	-- TODO: this doesnt work!?
+	vim.g.dashboard_custom_shortcut = {
+	    a = 'f',
+	    b = 'SPC f a',
+	    c = 'SPC s l',
+	    d = 'SPC c n',
+	    e = 'SPC f b'
+	}
+	-- find_history = 'SPC f h',
+
+    -- vim.g.dashboard_session_directory = CACHE_PATH..'/session'
+    vim.g.dashboard_custom_footer = O.dashboard.footer
+end
+
+return M
