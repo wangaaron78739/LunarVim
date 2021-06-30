822a3c782695d75e92df5781c10d418bafe836df
Lee Marlow <lee.marlow@gmail.com>
2021-06-30 10:17:11 -0600

    Use vim-elixir when elixir lang is active (#559)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -425,6 +425,12 @@ return require("packer").startup(function(use)
     -- TODO: use lazy loading maybe?
     use {"simrat39/rust-tools.nvim", disable = not O.lang.rust.active}
 
+    -- Elixir
+    use {"elixir-editors/vim-elixir",
+        ft = {"elixir", "eelixir"},
+        disable = not O.lang.elixir.active
+    }
+    
     -- use {'mattn/webapi-vim', opt = true}
     --     use {'f-person/git-blame.nvim', opt = true}
     --     -- diagnostics
