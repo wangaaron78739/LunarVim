b5e9fc6d538f35dd3cd8a67a4dd9b5d2639ba5c8
Anshuman <amedhi@connect.ust.hk>
2021-07-01 20:22:01 +0800

    this should be commented

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -89,25 +89,25 @@ vim.cmd([[
 autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
 ]])
 
-require'lspconfig'.rust_analyzer.setup{
-    cmd = {"rust-analyzer"},
-    on_attach = require'lsp'.common_on_attach,
-    settings = {
-        ["rust-analyzer"] = {
-            assist = {
-                importGranularity = "module",
-                importPrefix = "by_self",
-            },
-            cargo = {
-                loadOutDirsFromCheck = true
-            },
-            procMacro = {
-                enable = true
-            },
-            checkOnSave = {
-                enable = true,
-                command = "clippy" -- comment out to not use clippy
-            }
-        }
-    }
-}
+-- require'lspconfig'.rust_analyzer.setup{
+--     cmd = {"rust-analyzer"},
+--     on_attach = require'lsp'.common_on_attach,
+--     settings = {
+--         ["rust-analyzer"] = {
+--             assist = {
+--                 importGranularity = "module",
+--                 importPrefix = "by_self",
+--             },
+--             cargo = {
+--                 loadOutDirsFromCheck = true
+--             },
+--             procMacro = {
+--                 enable = true
+--             },
+--             rustcheckOnSave = {
+--                 enable = true,
+--                 command = "clippy" -- comment out to not use clippy
+--             }
+--         }
+--     }
+-- }
