e7535f0e25f8e0f13dcc3f02a30656a803f33f2e
Anshuman <amedhi@connect.ust.hk>
2021-07-01 16:35:20 +0800

    move telescope buffers

--- lua/lv-which-key/init.lua
+++ lua/lv-which-key/init.lua
@@ -118,6 +118,7 @@ local mappings = {
     },
     b = {
         name = "Buffers",
+        B = {"<cmd>Telescope buffers<cr>", "Buffers"},
         w = {":w<CR>", "Write"},
         a = {":wa<CR>", "Write All"},
         c = {":BufferClose<CR>", "Close"},
@@ -225,7 +226,6 @@ local mappings = {
     s = {
         name = "Search",
         b = {"<cmd>Telescope git_branches<cr>", "Checkout branch"},
-        B = {"<cmd>Telescope buffers<cr>", "Buffers"},
         c = {"<cmd>Telescope colorscheme<cr>", "Colorscheme"},
         d = {"<cmd>Telescope lsp_document_diagnostics<cr>", "Document Diagnostics"},
         D = {"<cmd>Telescope lsp_workspace_diagnostics<cr>", "Workspace Diagnostics"},
