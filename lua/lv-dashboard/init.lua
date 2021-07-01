be383f42b7b3636a5b31c5869ad1d91203b57477
Anshuman <amedhi@connect.ust.hk>
2021-07-01 16:42:20 +0800

    Refactor autocommands to go where they are really relevant

--- lua/lv-dashboard/init.lua
+++ lua/lv-dashboard/init.lua
@@ -1,24 +1,6 @@
 local M = {}
 
 M.config = function()
-    -- vim.g.dashboard_custom_header = {
-    --     '███╗   ██╗██╗   ██╗ ██████╗ ██████╗ ██████╗ ███████╗',
-    --     '████╗  ██║██║   ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝',
-    --     '██╔██╗ ██║██║   ██║██║     ██║   ██║██║  ██║█████╗',
-    --     '██║╚██╗██║╚██╗ ██╔╝██║     ██║   ██║██║  ██║██╔══╝',
-    --     '██║ ╚████║ ╚████╔╝ ╚██████╗╚██████╔╝██████╔╝███████╗',
-    --     '╚═╝  ╚═══╝  ╚═══╝   ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝'
-    -- }
-    -- vim.g.dashboard_custom_header = {
-    --
-    -- '      _..._                                                                           ',
-    -- '    .\'     `.   ██      ██    ██ ███    ██  █████  ██████  ██    ██ ██ ███    ███ ',
-    -- '   :         :  ██      ██    ██ ████   ██ ██   ██ ██   ██ ██    ██ ██ ████  ████ ',
-    -- '   :         :  ██      ██    ██ ██ ██  ██ ███████ ██████  ██    ██ ██ ██ ████ ██ ',
-    -- '   `.       .\'  ██      ██    ██ ██  ██ ██ ██   ██ ██   ██  ██  ██  ██ ██  ██  ██ ',
-    -- '     `-...-\'    ███████  ██████  ██   ████ ██   ██ ██   ██   ████   ██ ██      ██ ',
-    --
-    -- }
     vim.g.dashboard_disable_at_vimenter = 0
 
     vim.g.dashboard_custom_header = O.dashboard.custom_header
@@ -55,18 +37,22 @@ M.config = function()
 
     -- file_browser = {description = {' File Browser'}, command = 'Telescope find_files'},
 
-	-- TODO: this doesnt work!?
-	vim.g.dashboard_custom_shortcut = {
-	    a = 'f',
-	    b = 'SPC f a',
-	    c = 'SPC s l',
-	    d = 'SPC c n',
-	    e = 'SPC f b'
-	}
-	-- find_history = 'SPC f h',
 
     -- vim.g.dashboard_session_directory = CACHE_PATH..'/session'
     vim.g.dashboard_custom_footer = O.dashboard.footer
 end
+
+require('lv-utils').define_augroups({
+    _dashboard = {
+        -- seems to be nobuflisted that makes my stuff disapear will do more testing
+        {
+            'FileType', 'dashboard',
+            'setlocal nocursorline noswapfile synmaxcol& signcolumn=no norelativenumber nocursorcolumn nospell  nolist  nonumber bufhidden=wipe colorcolumn= foldcolumn=0 matchpairs= '
+        }, {
+            'FileType', 'dashboard',
+            'set showtabline=0 | autocmd BufLeave <buffer> set showtabline=2'
+        }, {'FileType', 'dashboard', 'nnoremap <silent> <buffer> q :q<CR>'}
+    }
+})
 
 return M
