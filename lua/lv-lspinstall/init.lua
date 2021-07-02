33b038e23bcec0a7faa774f0da5446959f768e53
Anshuman <amedhi@connect.ust.hk>
2021-07-02 20:42:22 +0800

    some bug here

--- lua/lv-lspinstall/init.lua
+++ lua/lv-lspinstall/init.lua
@@ -42,11 +42,11 @@ local function setup_servers()
     for _, server in pairs(servers) do require'lspconfig'[server].setup {} end
 end
 
-setup_servers()
+-- setup_servers()
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function()
-    setup_servers() -- reload installed servers
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
+-- require'lspinstall'.post_install_hook = function()
+--     setup_servers() -- reload installed servers
+--     vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+-- end
 
