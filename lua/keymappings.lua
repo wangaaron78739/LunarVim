46026e9af0c912185f91bb1e871ba0572efaabee
Anshuman <amedhi@connect.ust.hk>
2021-07-04 01:47:49 +0800

    cleaner keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,73 +1,80 @@
+local map = vim.api.nvim_set_keymap
+local opts = {silent = true}
+local nore = {noremap = true, silent = true}
+local expr = {noremap = true, silent = true, expr = true}
+
 -- TODO: change all the vim.cmd to vim.api.nvim_set_keymap
--- vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', nore)
 -- better window movement
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
 -- TODO fix this
 -- Terminal window navigation
-vim.cmd([[
-  tnoremap <C-h> <C-\><C-N><C-w>h
-  tnoremap <C-j> <C-\><C-N><C-w>j
-  tnoremap <C-k> <C-\><C-N><C-w>k
-  tnoremap <C-l> <C-\><C-N><C-w>l
-  inoremap <C-h> <C-\><C-N><C-w>h
-  inoremap <C-j> <C-\><C-N><C-w>j
-  inoremap <C-k> <C-\><C-N><C-w>k
-  inoremap <C-l> <C-\><C-N><C-w>l
-  tnoremap <Esc> <C-\><C-n>
-]])
+map('t', '<C-h>', [[<C-\><C-N><C-w>h]], nore)
+map('t', '<C-j>', [[<C-\><C-N><C-w>j]], nore)
+map('t', '<C-k>', [[<C-\><C-N><C-w>k]], nore)
+map('t', '<C-l>', [[<C-\><C-N><C-w>l]], nore)
+map('i', '<C-h>', [[<C-\><C-N><C-w>h]], nore)
+map('i', '<C-j>', [[<C-\><C-N><C-w>j]], nore)
+map('i', '<C-k>', [[<C-\><C-N><C-w>k]], nore)
+map('i', '<C-l>', [[<C-\><C-N><C-w>l]], nore)
+map('t', '<Esc>', [[<C-\><C-n>]], nore)
+-- vim.cmd([[
+--   tnoremap <C-h> <C-\><C-N><C-w>h
+--   tnoremap <C-j> <C-\><C-N><C-w>j
+--   tnoremap <C-k> <C-\><C-N><C-w>k
+--   tnoremap <C-l> <C-\><C-N><C-w>l
+--   inoremap <C-h> <C-\><C-N><C-w>h
+--   inoremap <C-j> <C-\><C-N><C-w>j
+--   inoremap <C-k> <C-\><C-N><C-w>k
+--   inoremap <C-l> <C-\><C-N><C-w>l
+--   tnoremap <Esc> <C-\><C-n>
+-- ]])
 
 -- TODO fix this
 -- resize with arrows
-vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -2<CR>', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +2<CR>', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>',
-                        {silent = true})
-vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>',
-                        {silent = true})
+map('n', '<C-Up>', ':resize -2<CR>', opts)
+map('n', '<C-Down>', ':resize +2<CR>', opts)
+map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
 -- better indenting
-vim.api.nvim_set_keymap('n', '<', 'v<', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '>', 'v>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'g<', '<', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'g>', '>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+map('n', '<', 'v<', nore)
+map('n', '>', 'v>', nore)
+map('n', 'g<', '<', nore)
+map('n', 'g>', '>', nore)
+map('v', '<', '<gv', nore)
+map('v', '>', '>gv', nore)
 
 -- I hate escape
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+map('i', 'jk', '<ESC>', nore)
+map('i', 'kj', '<ESC>', nore)
 
 -- Tab switch buffer
-vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>',
-                        {noremap = true, silent = true})
+map('n', '<TAB>', ':bnext<CR>', nore)
+map('n', '<S-TAB>', ':bprevious<CR>', nore)
 
 -- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv=gv',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv=gv',
-                        {noremap = true, silent = true})
-
--- Better nav for omnicomplete
-vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
-vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+map('x', 'K', ':move \'<-2<CR>gv=gv', nore)
+map('x', 'J', ':move \'>+1<CR>gv=gv', nore)
 
 -- Preserve register on pasting in visual mode
-vim.api.nvim_set_keymap('v', 'p', 'pgvy', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', 'P', 'p', {noremap = true, silent = true}) -- for normal p behaviour
--- vim.cmd('vnoremap P "0P')
--- vim.api.nvim_set_keymap('v', 'p', '"0p', {silent = true})
--- vim.api.nvim_set_keymap('v', 'P', '"0P', {silent = true})
+map('v', 'p', 'pgvy', nore)
+map('v', 'P', 'p', nore) -- for normal p behaviour
 
--- Completion window navigation
-vim.cmd('inoremap <expr> <TAB> (\"\\<C-n>\")')
-vim.cmd('inoremap <expr> <S-TAB> (\"\\<C-p>\")')
+-- Better nav for omnicomplete
+map('i', '<c-j>', '(\"\\<C-n>\")', expr)
+-- vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+map('i', '<TAB>', '(\"\\<C-n>\")', expr)
+-- vim.cmd('inoremap <expr> <TAB> (\"\\<C-n>\")')
+map('i', '<c-k>', '(\"\\<C-p>\")', expr)
+-- vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+map('i', '<S-TAB>', '(\"\\<C-p>\")', expr)
+-- vim.cmd('inoremap <expr> <S-TAB> (\"\\<C-p>\")')
 
--- vim.api.nvim_set_keymap('i', '<C-TAB>', 'compe#complete()', {noremap = true, silent = true, expr = true})
+-- map('i', '<C-TAB>', 'compe#complete()', {noremap = true, silent = true, expr = true})
 
 -- vim.cmd([[
 -- map p <Plug>(miniyank-autoput)
@@ -77,53 +84,44 @@ vim.cmd('inoremap <expr> <S-TAB> (\"\\<C-p>\")')
 -- ]])
 
 -- Toggle the QuickFix window
-vim.api.nvim_set_keymap('', '<C-q>', ':call QuickFixToggle()<CR>',
-                        {noremap = true, silent = true})
+map('', '<C-q>', ':call QuickFixToggle()<CR>', nore)
 
 -- Yank till end of the line 
-vim.api.nvim_set_keymap('n', 'Y', 'yg_', {noremap = true, silent = true})
+map('n', 'Y', 'yg_', nore)
 
 -- Go Back
-vim.api.nvim_set_keymap('n', 'gb', '<c-o>', {silent = true})
+map('n', 'gb', '<c-o>', opts)
 
 -- comment and copy
-vim.api.nvim_set_keymap('n', 'gyy', 'yy:CommentToggle<cr>p',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', 'gy',
-                        'ygv:<c-u>call CommentOperator(visualmode())<cr>`>p',
-                        {noremap = true, silent = true})
+map('n', 'gyy', 'yy:CommentToggle<cr>p', nore)
+map('v', 'gy', 'ygv:<c-u>call CommentOperator(visualmode())<cr>`>p', nore)
 
 -- Select Jupyter Cell
-vim.api.nvim_set_keymap('v', 'ic', [[/#+\s*%+<cr>oN]],
-                        {noremap = true, silent = true})
+map('v', 'ic', [[/#+\s*%+<cr>oN]], nore)
 
 -- Close FTerm
-vim.api.nvim_set_keymap('t', '<A-q>',
-                        '<C-\\><C-n><CMD>lua require("FTerm").close()<CR>',
-                        {noremap = true, silent = true})
+map('t', '<A-q>', '<C-\\><C-n><CMD>lua require("FTerm").close()<CR>', nore)
 
 -- Format buffer
-vim.api.nvim_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<cr>',
-                        {noremap = true, silent = true})
+map('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<cr>', nore)
 
 -- Spell checking
-vim.api.nvim_set_keymap('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u]]',
-                        {noremap = true, silent = true})
+map('i', '<C-l>', '<c-g>u<Esc>[s1z=`]a<c-g>u]]', nore)
 
 -- Vscode style commenting in insert mode
-vim.api.nvim_set_keymap('i', '<C-/>', '<C-\\><C-n><CMD>CommentToggle',
-                        {noremap = true, silent = true})
+map('i', '<C-/>', '<C-\\><C-n><CMD>CommentToggle', nore)
 
 -- Fix gcc keymapping
 -- vim.api.nvim_del_keymap('n', 'gc')
-vim.api.nvim_set_keymap('n', 'gcc', '<cmd>CommentToggle<cr>',
-                        {noremap = true, silent = true})
+map('n', 'gcc', '<cmd>CommentToggle<cr>', nore)
 
 -- Visual mode start search (like *)
-vim.api.nvim_set_keymap('v', '*', '"ay/<C-R>a<cr>',
-                        {noremap = true, silent = true})
+map('v', '*', '"ay/<C-R>a<cr>', nore)
 
 -- peek definition
-vim.api.nvim_set_keymap('n', 'gpd', ':Lspsaga preview_definition<cr>',
-                        {silent = true})
+map('n', 'gpd', ':Lspsaga preview_definition<cr>', opts)
 
+if O.plugin.ts_hintobjects.active then
+    map('o', 'm', [[:<C-U>lua require('tsht').nodes()<CR>]], opts)
+    map('v', 'm', [[:lua require('tsht').nodes()<CR>]], nore)
+end
