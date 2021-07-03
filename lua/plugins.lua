9c60a600526070140cf445299ec2491cb04f7027
Anshuman <amedhi@connect.ust.hk>
2021-07-04 04:33:11 +0800

    use lightspeed instead of sneak

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -511,7 +511,26 @@ return require("packer").startup(function(use)
     use {"unblevable/quick-scope", disable = not O.plugin.quickscope.active}
 
     -- 2 letter find
-    use {"justinmk/vim-sneak", disable = not O.plugin.sneak.active}
+    -- use {"justinmk/vim-sneak", disable = not O.plugin.sneak.active}
+    use {
+        "ggandor/lightspeed.nvim",
+        config = function()
+            require'lightspeed'.setup {
+                jump_to_first_match = true,
+                jump_on_partial_input_safety_timeout = 400,
+                highlight_unique_chars = true,
+                grey_out_search_area = true,
+                match_only_the_start_of_same_char_seqs = true,
+                limit_ft_matches = 5,
+                full_inclusive_prefix_key = '<c-x>',
+                -- By default, the values of these will be decided at runtime,
+                -- based on `jump_to_first_match`
+                labels = nil,
+                cycle_group_fwd_key = nil,
+                cycle_group_bwd_key = nil
+            }
+        end, disable = not O.plugin.sneak.active
+    }
 
     -- Multi cursor support
     use {"mg979/vim-visual-multi", disable = not O.plugin.visual_multi.active}
