986dd098e96109e216c69f11a742156f8a4cf2bf
Anshuman <amedhi@connect.ust.hk>
2021-07-04 17:58:57 +0800

    restore example config

--- utils/installer/lv-config.example.lua
+++ utils/installer/lv-config.example.lua
@@ -8,30 +8,37 @@ an executable
 ]] -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 -- general
 O.auto_complete = true
-O.colorscheme = 'snazzy'
+O.colorscheme = 'spacegray'
 O.auto_close_tree = 0
 O.wrap_lines = false
 O.timeoutlen = 100
 O.document_highlight = true
-O.extras = true
+O.extras = false
 O.leader_key = ' '
 O.ignore_case = true
 O.smart_case = true
-O.shell = 'fish'
 O.lushmode = false
 O.transparent_window = false
 
 -- After changing plugin config it is recommended to run :PackerCompile
--- TODO: refine these selections
-O.plugin.lsp_rooter.active = false -- This is actually more confusing sometimes
+O.plugin.hop.active = false
+O.plugin.dial.active = false
+O.plugin.dashboard.active = true
+O.plugin.matchup.active = false
+O.plugin.colorizer.active = false
+O.plugin.numb.active = false
+O.plugin.ts_playground.active = false
+O.plugin.indent_line.active = false
+O.plugin.gitlinker.active = false
+O.plugin.zen.active = false
 
 -- dashboard
 -- O.dashboard.custom_header = {""}
-O.dashboard.footer = {"Anshuman Medhi (IndianBoy42)"}
+-- O.dashboard.footer = {""}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = "all"
--- O.treesitter.ignore_install = {"haskell"}
+O.treesitter.ignore_install = {"haskell"}
 O.treesitter.highlight.enabled = true
 
 O.lang.clang.diagnostics.virtual_text = true
@@ -42,7 +49,7 @@ O.lang.clang.diagnostics.underline = true
 -- add things like O.python.formatter.yapf.exec_path
 -- add things like O.python.linter.flake8.exec_path
 -- add things like O.python.formatter.isort.exec_path
-O.lang.python.formatter = 'black'
+O.lang.python.formatter = 'yapf'
 -- O.python.linter = 'flake8'
 O.lang.python.isort = true
 O.lang.python.autoformat = true
@@ -53,14 +60,16 @@ O.lang.python.analysis.type_checking = "off"
 O.lang.python.analysis.auto_search_paths = true
 O.lang.python.analysis.use_library_code_types = true
 
+
 -- lua
 -- TODO look into stylua
 O.lang.lua.formatter = 'lua-format'
-O.lang.lua.autoformat = true
+-- O.lua.formatter = 'lua-format'
+O.lang.lua.autoformat = false
 
 -- javascript
 O.lang.tsserver.formatter = 'prettier'
-O.lang.tsserver.linter = 'eslint'
+O.lang.tsserver.linter = nil
 O.lang.tsserver.autoformat = true
 
 -- json
@@ -73,12 +82,12 @@ O.lang.ruby.autoformat = true
 O.lang.go.autoformat = true
 
 -- rust
-O.lang.rust.active = true
 O.lang.rust.autoformat = true
 
--- latex
-O.lang.latex.active = true
-O.lang.latex.autoformat = true
+-- clang
+O.lang.clang.autoformat = false -- Set to true to enable auto-format in C/C++ files.
+
+
 
 -- create custom autocommand field (This would be easy with lua)
 
@@ -91,23 +100,18 @@ O.lang.latex.autoformat = true
 -- Neovim turns the default cursor to 'Block'
 -- when switched back into terminal.
 -- This below line fixes that. Uncomment if needed.
+
 -- vim.cmd('autocmd VimLeave,VimSuspend * set guicursor=a:ver90') -- Beam
 -- vim.cmd('autocmd VimLeave,VimSuspend * set guicursor=a:hor20') -- Underline
+
 -- NOTE: Above code doesn't take a value from the terminal's cursor and
 --       replace it. It hardcodes the cursor shape.
 --       And I think `ver` means vertical and `hor` means horizontal.
 --       The numbers didn't make a difference in alacritty. Please change
 --       the number to something that suits your needs if it looks weird.
 
--- neovide settings
--- vim.g.neovide_cursor_vfx_mode = "railgun"
--- vim.g.neovide_refresh_rate=120
-
--- My settings
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.scrolloff = 10
-
--- Turn off cursorline
--- O.cursorline = false
+-- TODO
+-- custom autocommands
+-- user defined plugin config
+-- custom settings
 
