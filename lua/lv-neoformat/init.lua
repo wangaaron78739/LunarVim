1d57e90e37e45721654752d4ed89811096069b35
Anshuman <amedhi@connect.ust.hk>
2021-07-06 07:26:37 +0800

    this mess up my justfiles

--- lua/lv-neoformat/init.lua
+++ lua/lv-neoformat/init.lua
@@ -2,12 +2,13 @@ local M = {}
 
 M.config = function()
   vim.g.neoformat_enabled_lua = { "stylua", "luaformat" }
-  -- Enable alignment
+
+  --[[ -- Enable alignment
   vim.g.neoformat_basic_format_align = 1
   -- Enable tab to spaces conversion
   vim.g.neoformat_basic_format_retab = 1
   -- Enable trimmming of trailing whitespace
-  vim.g.neoformat_basic_format_trim = 1
+  vim.g.neoformat_basic_format_trim = 1 ]]
 end
 
 return M
