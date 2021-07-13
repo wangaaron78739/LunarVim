f8444fad43045da54e01c8bab7874fe89b0f7f8e
Anshuman <amedhi@connect.ust.hk>
2021-07-13 23:37:21 +0800

    clangd_flags

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -2,7 +2,7 @@ if require("lv-utils").check_lsp_client_active "clangd" then
   return
 end
 
-local clangd_flags = { "--background-index", "--query-driver=**/arm-none-eabi-*,**/x86_64-linux-**" }
+local clangd_flags = { "--background-index", "--query-driver=**/arm-none-eabi-*,**/x86_64-linux-*" }
 
 if O.lang.clang.cross_file_rename then
   table.insert(clangd_flags, "--cross-file-rename")
