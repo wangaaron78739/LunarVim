52901163c397b4cf76e8832b3d9e9822d1e69fa5
Anshuman <amedhi@connect.ust.hk>
2021-08-08 02:29:51 +0800

    "Fix" wilder + vim-visual-multi

--- lua/lv-wilder/init.lua
+++ lua/lv-wilder/init.lua
@@ -1,11 +1,18 @@
 local M = {}
 
+-- TODO: Convert to lua
 function M.config()
-  vim.cmd [[call wilder#enable_cmdline_enter()]]
+  -- vim.cmd [[call wilder#enable_cmdline_enter()]]
+  vim.api.nvim_set_keymap("c", "<F1>", "wilder#main#start()", { expr = true, silent = true })
+  vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
+  vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+  vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
+  vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+  vim.cmd [[call wilder#set_option('modes', ['/', '?', ':'])]]
   vim.cmd [[set wildcharm=<Tab>]]
   vim.cmd [[cmap <expr> <Tab> wilder#in_context() ? wilder#next() : "\<Tab>"]]
   vim.cmd [[cmap <expr> <S-Tab> wilder#in_context() ? wilder#previous() : "\<S-Tab>"]]
-  vim.cmd [[call wilder#set_option('modes', ['/', '?', ':'])]]
+  -- vim.cmd [[call wilder#set_option('modes', [':'])]]
 
   vim.cmd [[
   call wilder#set_option('pipeline', [ wilder#branch( wilder#cmdline_pipeline({ 'fuzzy': 1, }), wilder#python_search_pipeline({ 'pattern': 'fuzzy', }),), ])
