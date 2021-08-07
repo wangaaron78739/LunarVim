72c2403a1979ae8e0bf6b33656072cd449ab8e8f
Anshuman <amedhi@connect.ust.hk>
2021-08-08 04:57:06 +0800

    debugging wilder.nvim

--- lua/lv-wilder/init.lua
+++ lua/lv-wilder/init.lua
@@ -2,25 +2,30 @@ local M = {}
 
 -- TODO: Convert to lua
 function M.config()
-  -- vim.cmd [[call wilder#enable_cmdline_enter()]]
-  vim.api.nvim_set_keymap("c", "<F1>", "wilder#main#start()", { expr = true, silent = true })
-  vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
-  vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
-  vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
-  vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+  vim.cmd [[call wilder#enable_cmdline_enter()]]
+  -- vim.api.nvim_set_keymap("c", "<F1>", "wilder#main#start()", { expr = true, silent = true })
+  -- vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
+  -- vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+  -- vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
+  -- vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+
   vim.cmd [[call wilder#set_option('modes', ['/', '?', ':'])]]
+  -- vim.cmd [[call wilder#set_option('modes', [':'])]]
   vim.cmd [[set wildcharm=<Tab>]]
   vim.cmd [[cmap <expr> <Tab> wilder#in_context() ? wilder#next() : "\<Tab>"]]
   vim.cmd [[cmap <expr> <S-Tab> wilder#in_context() ? wilder#previous() : "\<S-Tab>"]]
-  -- vim.cmd [[call wilder#set_option('modes', [':'])]]
 
   vim.cmd [[
   call wilder#set_option('pipeline', [ wilder#branch( wilder#cmdline_pipeline({ 'fuzzy': 1, }), wilder#python_search_pipeline({ 'pattern': 'fuzzy', }),), ])
     ]]
 
+--   vim.cmd [[
+-- let g:wilder_highlighters = [ wilder#pcre2_highlighter(), wilder#basic_highlighter(), ] 
+-- call wilder#set_option('renderer', wilder#renderer_mux({ ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), '/': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), }))
+--     ]]
   vim.cmd [[
 let g:wilder_highlighters = [ wilder#pcre2_highlighter(), wilder#basic_highlighter(), ] 
-call wilder#set_option('renderer', wilder#renderer_mux({ ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), '/': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), }))
+call wilder#set_option('renderer', wilder#renderer_mux({ ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), '/': wilder#wildmenu_renderer({'mode': 'statusline'}), }))
     ]]
 end
 
