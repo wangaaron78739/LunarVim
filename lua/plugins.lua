822464acf30b5d8a7858394a34f4f65c748927bb
Anshuman <amedhi@connect.ust.hk>
2021-08-11 21:38:56 +0800

    fix magma operator

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -672,7 +672,7 @@ return require("packer").startup(function(use)
     "dccsillag/magma-nvim",
     config = function()
       local remap = vim.api.nvim_set_keymap
-      remap("n", "gx", "nvim_exec('MagmaEvaluateOperator', v:true)", { expr = true, silent = true })
+      remap("n", "gx", ":MagmaEvaluateOperator<CR>", { expr = true, silent = true })
       remap("v", "gx", ":<C-u>MagmaEvaluateVisual<CR>", { silent = true })
       remap("n", "gxx", "<cmd>MagmaEvaluateLine<CR>", { silent = true })
       vim.g.magma_automatically_open_output = false
