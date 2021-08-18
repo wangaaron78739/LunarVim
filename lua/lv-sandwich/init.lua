449c5783100bca0f886194769b0e7812c74cd166
Anshuman <amedhi@connect.ust.hk>
2021-08-18 17:04:39 +0800

    sandwich helpers and config

--- lua/lv-sandwich/init.lua
+++ lua/lv-sandwich/init.lua
@@ -1,11 +1,34 @@
 local M = {}
+
+M.recipes = {}
+function M.add_recipe(recipe)
+  vim.list_extend(M.recipes, { recipe })
+  vim.g["sandwich#recipes"] = M.recipes
+  -- table.insert(vim.g["sandwich#recipes"], recipe)
+  -- vim.g["sandwich#recipes"]:append(recipe)
+end
+
 function M.preconf()
   vim.g.sandwich_no_default_key_mappings = 1
   vim.g.operator_sandwich_no_default_key_mappings = 1
   vim.g.textobj_sandwich_no_default_key_mappings = 1
 end
+
 function M.config()
-  vim.api.nvim_command "runtime macros/sandwich/keymap/surround.vim"
+  vim.cmd "runtime macros/sandwich/keymap/surround.vim"
+
+  -- vim.tbl_extend(M.recipes, vim.g["sandwich#recipes"])
+  M.recipes = vim.g["sandwich#recipes"]
+
+  -- TODO: use inline_text_input for sandwich function
+  --   M.add_recipe {
+  --     buns = { "lua require('lv-sandwich').fname()", '")"' },
+  --     expr = true,
+  --     kind = { "add", "replace" },
+  --     action = { "add" },
+  --     input = { "f" },
+  --   }
+  --
   vim.api.nvim_command [[
       xmap is <Plug>(textobj-sandwich-query-i)
       xmap as <Plug>(textobj-sandwich-query-a)
@@ -13,4 +36,11 @@ function M.config()
       omap as <Plug>(textobj-sandwich-query-a)
   ]]
 end
+
+function M.fname()
+  vim.fn["operator#sandwich#show"]()
+
+  vim.fn["operator#sandwich#quench"]()
+end
+
 return M
