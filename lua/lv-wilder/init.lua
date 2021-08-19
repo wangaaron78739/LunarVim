d9637e41620325e24d6bab548aefbf81f73c694a
Anshuman <amedhi@connect.ust.hk>
2021-08-19 11:37:59 +0800

    wilder convert to lua

--- lua/lv-wilder/init.lua
+++ lua/lv-wilder/init.lua
@@ -1,32 +1,51 @@
 local M = {}
 
+M.wilder = setmetatable({}, {
+  __index = function(mytable, key)
+    return vim.fn["wilder#" .. key]
+  end,
+})
+M.set_option = setmetatable({}, {
+  __index = function(mytable, key)
+    return function(opts)
+      M.wilder.set_option(key, opts)
+    end
+  end,
+  __newindex = function(table, key, value)
+    M.wilder.set_option(key, value)
+  end,
+})
+M.options = setmetatable({}, {
+  __newindex = function(mytable, key, value)
+    vim.cmd([[call wilder#set_option(']] .. key .. "', " .. value:gsub("\n", "") .. ")")
+  end,
+})
+
 -- TODO: Convert to lua
 function M.config()
-  vim.cmd [[call wilder#enable_cmdline_enter()]]
+  M.wilder.enable_cmdline_enter()
+
+  M.set_option.modes = { "/", "?", ":" }
+
+  vim.opt.wildcharm = vim.fn.char2nr "	" -- This is a <TAB>
+  -- vim.api.nvim_replace_termcodes("<tab>", true, true, true)
+
   -- vim.api.nvim_set_keymap("c", "<F1>", "wilder#main#start()", { expr = true, silent = true })
-  -- vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
-  -- vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
-  -- vim.api.nvim_set_keymap("n", "g/", "/", { noremap = true })
-  -- vim.api.nvim_set_keymap("n", "/", "g/<F1>", {})
+  vim.api.nvim_set_keymap("c", "<Tab>", [[wilder#in_context() ? wilder#next() : "\<Tab>"]], { expr = true })
+  vim.api.nvim_set_keymap("c", "<S-Tab>", [[wilder#in_context() ? wilder#previous() : "\<S-Tab>"]], { expr = true })
 
-  vim.cmd [[call wilder#set_option('modes', ['/', '?', ':'])]]
-  -- vim.cmd [[call wilder#set_option('modes', [':'])]]
-  vim.cmd [[set wildcharm=<Tab>]]
-  vim.cmd [[cmap <expr> <Tab> wilder#in_context() ? wilder#next() : "\<Tab>"]]
-  vim.cmd [[cmap <expr> <S-Tab> wilder#in_context() ? wilder#previous() : "\<S-Tab>"]]
+  M.options.pipeline = [[ 
+  [ wilder#branch( wilder#cmdline_pipeline({ 'fuzzy': 1, }), wilder#python_search_pipeline({ 'pattern': 'fuzzy', }),), ] 
+  ]]
 
-  vim.cmd [[
-  call wilder#set_option('pipeline', [ wilder#branch( wilder#cmdline_pipeline({ 'fuzzy': 1, }), wilder#python_search_pipeline({ 'pattern': 'fuzzy', }),), ])
-    ]]
+  vim.g.wilder_highlighters = { M.wilder.pcre2_highlighter(), M.wilder.basic_highlighter() }
 
---   vim.cmd [[
--- let g:wilder_highlighters = [ wilder#pcre2_highlighter(), wilder#basic_highlighter(), ] 
--- call wilder#set_option('renderer', wilder#renderer_mux({ ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), '/': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), }))
---     ]]
-  vim.cmd [[
-let g:wilder_highlighters = [ wilder#pcre2_highlighter(), wilder#basic_highlighter(), ] 
-call wilder#set_option('renderer', wilder#renderer_mux({ ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter(), }), '/': wilder#wildmenu_renderer({'mode': 'statusline'}), }))
-    ]]
+  M.options.renderer = [[
+     wilder#renderer_mux({ 
+       ':': wilder#popupmenu_renderer({ 'highlighter': wilder#basic_highlighter() }), 
+       '/': wilder#wildmenu_renderer({ 'mode': 'statusline' }), 
+     })
+  ]]
 end
 
 return M
