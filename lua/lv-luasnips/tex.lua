3013d35105195eec7e935aa329e22fa9a19ead0d
Anshuman <amedhi@connect.ust.hk>
2021-09-02 12:39:05 +0800

    trying to port latex snippets

--- lua/lv-luasnips/tex.lua
+++ lua/lv-luasnips/tex.lua
@@ -1,3 +1,6 @@
+local F = require "F"
+local L = require("pl.utils").string_lambda
+
 -- some shorthands...
 local ls = require "luasnip"
 local s = ls.snippet
@@ -16,6 +19,7 @@ local dl = require("luasnip.extras").dynamic_lambda
 local pa = ls.parser.parse_snippet
 local types = require "luasnip.util.types"
 local nl = t { "", "" }
+local list_extend = vim.list_extend
 
 local rec_ls
 rec_ls = function()
@@ -41,54 +45,158 @@ local function fmt(fn, ipairs)
     return string.format(unpack(fn(args[1].captures, args[1].trigger, args)))
   end, ipairs)
 end
+local sub = function(ix)
+  return f(function(args)
+    return string.format("%s", args[1].captures[ix])
+  end, {})
+end
+local con = function(fn)
+  return { condition = fn }
+end
+local mathmode_ = vim.fn["vimtex#syntax#in_mathzone"]
+local mathmode = {
+  condition = function()
+    return mathmode_() ~= 0
+  end,
+}
+local nonmathmode = {
+  condition = function()
+    return mathmode_() == 0
+  end,
+}
+local ms = function(lhs, rhs)
+  return s(lhs, rhs, mathmode)
+end
+local nms = function(lhs, rhs)
+  return s(lhs, rhs, nonmathmode)
+end
+local ff = function(str)
+  return f(function(args)
+    local c1 = args[1].captures[1]
+    local c2 = args[1].captures[2]
+    local c3 = args[1].captures[3]
+    local c4 = args[1].captures[4]
+    return L(str)
+  end, {})
+end
 
+local autosyms = {
+  "alpha",
+  "beta",
+  "gamma",
+  "delta",
+  "epsilon",
+  "zeta",
+  "eta",
+  "theta",
+  "iota",
+  "kappa",
+  "lambda",
+  "mu",
+  "nu",
+  "xi",
+  "omicron",
+  "pi",
+  "rho",
+  "sigma",
+  "tau",
+  "upsilon",
+  "phi",
+  "chi",
+  "psi",
+  "omega",
+  "Alpha",
+  "Beta",
+  "Gamma",
+  "Delta",
+  "Epsilon",
+  "Zeta",
+  "Eta",
+  "Theta",
+  "Iota",
+  "Kappa",
+  "Lambda",
+  "Mu",
+  "Nu",
+  "Xi",
+  "Omicron",
+  "Pi",
+  "Rho",
+  "Sigma",
+  "Tau",
+  "Upsilon",
+  "Phi",
+  "Chi",
+  "Psi",
+  "Omega",
+  "nabla",
+  "infty",
+}
+local autosyms_math = {}
+local autosyms_open = {}
+for j, v in ipairs(autosyms) do
+  autosyms_math[j] = ms(v, t("\\" .. v))
+  autosyms_open[j] = nms(v, t("$\\" .. v .. "$"))
+end
+local symmaps_table = {
+  ["..."] = "dots",
+  ["=>"] = "implies",
+  ["=<"] = "impliedby",
+  [">="] = "geq",
+  ["<="] = "leq",
+  ["!="] = "neq",
+  ["~="] = "approx",
+  ["~~"] = "sim",
+  [">>"] = "gg",
+  ["<<"] = "ll",
+  ["xx"] = "times",
+  ["**"] = "cdot",
+}
+
+local auto = {
+  s("$", { t "\\(", i(0), t "\\)" }),
+  s("\\(", { t "\\( ", i(0), t " \\" }),
+  s("\\it ", { t "\\textit{", i(0), t "}" }),
+  s("\\bf ", { t "\\textbf{", i(0), t "}" }),
+  s("\\eq ", { t "\\begin{equation}\n", i(0), t "\n\\end{equation}" }),
+  s("\\ali ", { t "\\begin{equation}\n", i(0), t "\n\\end{equation}" }),
+  s("--", { t "\\item " }),
+  s(re [[(%w+)%.%.e]], {
+    -- ff "\begin{{{c1}}}",
+    fmt(function(cap)
+      return { [[\begin{%s}]], cap[1] }
+    end),
+    nl,
+    i(0),
+    nl,
+    fmt(function(cap)
+      return { [[\end{%s}]], cap[1] }
+    end),
+  }),
+  ms(re [[(%S) ([%^_])]], { sub(1), sub(2) }), -- Remove extra ws sub/superscript
+  ms(re [[([A-Za-z%}%]%)])(%d)]], { sub(1), t "_", sub(2) }), -- Auto subscript
+  ms(re [[([A-Za-z%}%]%)]) ?_(%d%d)]], { sub(1), t "_{", sub(2), t "}" }), -- Auto escape subscript
+  ms(re [[([A-Za-z%}%]%)]) ?%^ ?(%d%d)]], { sub(1), t "^{", sub(2), t "}" }), -- Auto escape superscript
+  ms(re [[([A-Za-z%}%]%)]) ?%^([%+%-]? ?[%w]) ]], { sub(1), t "^{", sub(2), t "}" }), -- Auto escape superscript
+  ms(re [[([A-Za-z%}%]%)]) ?%^([%+%-]? ?%\%w+) ]], { sub(1), t "^{", sub(2), t "}" }), -- Auto escape superscript
+  -- TODO: whitespace before and after operators
+  -- TODO: fraction
+  -- TODO: line 203 and below
+}
+list_extend(auto, autosyms_math)
+list_extend(auto, autosyms_open)
+for k, v in pairs(symmaps_table) do
+  utils.dump { k, v }
+  list_extend(auto, { ms(k, t("\\" .. v)) })
+end
+
+local snips = {
+  s("\\theorem ", { t "\\begin{theorem}\n", i(0), t "\n\\end{theorem}" }),
+  s("\\lemma ", { t "\\begin{lemma}\n", i(0), t "\n\\end{lemma}" }),
+  s("\\proof ", { t "\\begin{proof}\n", i(0), t "\n\\end{proof}" }),
+  s("\\claim ", { t "\\begin{proof}\n", i(0), t "\n\\end{proof}" }),
+}
 return {
-  snips = {
-    s("ls", {
-      t { "\\begin{itemize}", "\t\\item " },
-      i(1),
-      d(2, rec_ls, {}),
-      t { "", "\\end{itemize}" },
-      i(0),
-    }),
-    s("\\theorem ", { t "\\begin{theorem}\n", i(0), t "\n\\end{theorem}" }),
-    s("\\lemma ", { t "\\begin{lemma}\n", i(0), t "\n\\end{lemma}" }),
-    s("\\proof ", { t "\\begin{proof}\n", i(0), t "\n\\end{proof}" }),
-    s("\\claim ", { t "\\begin{proof}\n", i(0), t "\n\\end{proof}" }),
-  },
-  auto = {
-    s("$", { t "\\(", i(0), t "\\)" }),
-    s("\\(", { t "\\( ", i(0), t " \\" }),
-    s("\\it ", { t "\\textit{", i(0), t "}" }),
-    s("\\bf ", { t "\\textbf{", i(0), t "}" }),
-    s("\\eq ", { t "\\begin{equation}\n", i(0), t "\n\\end{equation}" }),
-    s("\\ali ", { t "\\begin{equation}\n", i(0), t "\n\\end{equation}" }),
-    -- s(re [[e_(%w+) ]], {
-    s(re [[(%w+)%.%.e]], {
-      fmt(function(cap)
-        return { [[\begin{%s}]], cap[1] }
-      end),
-      nl,
-      i(0),
-      nl,
-      fmt(function(cap)
-        return { [[\end{%s}]], cap[1] }
-      end),
-    }),
-    s("--", { t "\\item " }),
-    -- The last entry of args passed to the user-function is the surrounding snippet.
-    s(
-      re [[hello(%w+) ]],
-      f(function(args)
-        return "Triggered with " .. args[1].trigger .. "."
-      end, {})
-    ),
-    -- It's possible to use capture-groups inside regex-triggers.
-    s(
-      { trig = "b(%d)", regTrig = true },
-      f(function(args)
-        return "Captured Text: " .. args[1].captures[1] .. "."
-      end, {})
-    ),
-  },
+  snips = snips,
+  auto = auto,
 }
