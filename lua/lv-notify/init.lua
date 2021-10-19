e71b077f5f1ae85e233181e580b8dd3a69723302
Anshuman <amedhi@connect.ust.hk>
2021-10-19 10:17:42 +0800

    simplify

--- lua/lv-notify/init.lua
+++ lua/lv-notify/init.lua
@@ -1,31 +1,7 @@
 local M = {}
-local notify = require "notify"
-local color = {
-  DEBUG = "NotifyDEBUGTitle",
-  TRACE = "NotifyTRACETitle",
-  INFO = "NotifyINFOTitle",
-  WARN = "NotifyWARNTitle",
-  ERROR = "NotifyERRORTitle",
-}
-M.print_history = function()
-  local echo = vim.api.nvim_echo
-  local strftime = vim.fn.strftime
-  local concat = table.concat
-  for _, m in ipairs(notify.history()) do
-    echo({
-      { strftime("%FT%T", m.time), "Identifier" },
-      { " ", "Normal" },
-      { m.level, color[m.level] or "Title" },
-      { " ", "Normal" },
-      { concat(m.message, " "), "Normal" },
-    }, false, {})
-  end
-end
 M.config = function()
-  vim.cmd [[command! Message :lua require'lv-notify'.print_history()<CR>]]
-  vim.cmd [[command! Mess :lua require'lv-notify'.print_history()<CR>]]
+  local notify = require "notify"
   notify.setup(O.notify)
-
   vim.notify = notify
 end
 return M
